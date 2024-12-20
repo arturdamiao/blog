bffbf9a73ef92f9767e5324e9dad19a63fe9ad90
arturdamiao <arturdamiao@hotmail.com.br>
2024-12-20 19:39:37 -0300

    Initial commit

---
+++ 00-start.R
@@ -0,0 +1,19 @@
+# 1) instalar o blogdown
+remotes::install_github('rstudio/blogdown')
+
+# 1.1) Verifique a versão instalada do Hugo. 
+blogdown::hugo_version()
+# > [1] ‘0.140.0’
+
+# 2) Criar o blog com blogdown, e tema Apero
+blogdown::new_site(theme = "hugo-apero/hugo-apero", 
+                   format = "toml",
+                   sample = FALSE,
+                   empty_dirs = TRUE)
+# 4) Serve site: 
+blogdown::serve_site()
+
+# 5) Criar um post:
+blogdown::new_post(title = "Olá mundo!", 
+                   ext = '.Rmarkdown', 
+                   subdir = "blog")
---
+++ content/blog/2024-12-20-ola-mundo/index.Rmarkdown
@@ -0,0 +1,12 @@
+---
+title: Olá mundo!
+author: ''
+date: '2024-12-20'
+slug: []
+categories: []
+tags: []
+---
+
+Escrevendo qualquer coisa para ver se funciona isso aqui. 
+
+Testando a escrita com **rmarkdown**. O que será que acontece?
